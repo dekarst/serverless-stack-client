70a206cdd8d299e540507ac5d58d90b348402ce3
dekarst <dekarsts@gmail.com>
2020-08-06 15:26:13 +0200

    signup

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,10 +1,148 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { Auth } from 'aws-amplify'
+import { useAppContext } from '../libs/contextLib'
+import { onError } from '../libs/errorLib'
+import useFormFields from '../hooks/useFormFields'
+import LoaderButton from '../components/LoaderButton'
 
-const Signup = () => (
-  <div>
-    <h1>Signup</h1>
-  </div>
-)
+const Form = styled.form`
+  width: 50%;
+  margin: 1em auto;
+  display: flex;
+  flex-direction: column;
+  & div {
+    display: flex;
+    justify-content: space-between;
+  }
+  & button {
+    text-align: center;
+    font-size: 1em;
+  }
+`
 
-export default Signup
+export default function Signup() {
+  const history = useHistory()
+  const { userHasAuthenticated } = useAppContext()
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [fields, setFields] = useFormFields({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    confirmationCode: '',
+  })
+  const [newUser, setNewUser] = useState(null)
+
+  const validateForm = () => (
+    fields.email.length > 0 &&
+    fields.password.length > 0 &&
+    fields.password === fields.confirmPassword
+  )
+
+  const validateConfirmationForm = () => fields.confirmationCode.length > 0
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setIsLoading(true)
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      })
+      setIsLoading(false)
+      setNewUser(newUser)
+    } catch (e) {
+      onError(e)
+      setIsLoading(false)
+    }
+  }
+
+  async function handleConfirmationSubmit(event) {
+    event.preventDefault()
+    setIsLoading(true)
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode)
+      await Auth.signIn(fields.email, fields.password)
+      userHasAuthenticated(true)
+      history.push('/')
+    } catch (e) {
+      onError(e)
+      setIsLoading(false)
+    }
+  }
+
+  const renderConfirmationForm = () => (
+    <Form onSubmit={handleConfirmationSubmit}>
+      <p>Check your email for the confirmation code.</p>
+      <div>
+        <label>Code</label>
+        <input
+          autoFocus
+          type='tel'
+          id='confirmationCode'
+          value={fields.confirmationCode}
+          onChange={setFields}
+        />
+      </div>
+      <LoaderButton
+        block
+        type='submit'
+        bsSize='large'
+        isLoading={isLoading}
+        disabled={!validateConfirmationForm()}
+      >
+        Verify
+      </LoaderButton>
+    </Form>
+  )
+
+  const renderForm = () => (
+    <Form onSubmit={handleSubmit}>
+      <div>
+        <label>Email</label>
+        <input
+          autoFocus
+          type='email'
+          id='email'
+          value={fields.email}
+          onChange={setFields}
+        />
+      </div>
+      <div>
+        <label>Pass</label>
+        <input
+          type='password'
+          id='password'
+          value={fields.password}
+          onChange={setFields}
+        />
+      </div>
+      <div>
+        <label>Pass</label>
+        <input
+          type='password'
+          id='confirmPassword'
+          onChange={setFields}
+          value={fields.confirmPassword}
+        />
+      </div>
+      <LoaderButton
+        block
+        type='submit'
+        bsSize='large'
+        isLoading={isLoading}
+        disabled={!validateForm()}
+      >
+        Signup
+      </LoaderButton>
+    </Form>
+  )
+
+  return (
+    <div className='Signup'>
+      {newUser === null ? renderForm() : renderConfirmationForm()}
+    </div>
+  )
+}
